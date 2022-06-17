f903ff99e123a6fd9f2eef0c39de11a83a6162f4
Andrej Szalma <andy.szalma@gmail.com>
2022-06-17 12:01:44 +0100

    removed apollo hermes from benchmarks

--- clients/index.ts
+++ clients/index.ts
@@ -1,6 +1,4 @@
-import { ApolloHermes } from './apollo-hermes';
-import { ApolloHermesStrict } from './apollo-hermes-strict';
 import { ApolloInMemory } from './apollo-inmemory';
 import { Relay } from './relay';
 
-export = [ApolloHermes, ApolloHermesStrict, ApolloInMemory, Relay];
+export = [ApolloInMemory, Relay];
