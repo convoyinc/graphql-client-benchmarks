7f5ba7215c514704ab302e7f89a99dc420b4b0ee
Andrej Szalma <andy.szalma@gmail.com>
2022-07-12 14:15:47 +0100

    updated to work universally with current repo

--- scripts/update-pages.sh
+++ scripts/update-pages.sh
@@ -4,11 +4,12 @@ set -x
 CURRENT_COMMIT=$(git rev-parse HEAD)
 
 rm -rf ./dist
-git clone -b gh-pages https://github.com/convoyinc/graphql-client-benchmarks ./dist
+git clone -b gh-pages ./ ./dist
 
 ./scripts/compile-web.sh
 
 cd ./dist
+git pull
 git add .
 git commit -m "Built web reporter for ${CURRENT_COMMIT}"
 git push origin gh-pages
