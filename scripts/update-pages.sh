5215732457aeb47db7845ed283ddc78e7e9fb0eb
Andrej Szalma <andy.szalma@gmail.com>
2022-07-15 16:35:14 +0100

    updated pages deploy script

--- scripts/update-pages.sh
+++ scripts/update-pages.sh
@@ -8,8 +8,12 @@ git clone -b gh-pages ./ ./dist
 
 ./scripts/compile-web.sh
 
+# get origin url of main repo
+REMOTE=$(git remote get-url origin)
+
 cd ./dist
-git pull
+git remote set-url origin $REMOTE
+git pull origin
 git add .
 git commit -m "Built web reporter for ${CURRENT_COMMIT}"
 git push origin gh-pages
