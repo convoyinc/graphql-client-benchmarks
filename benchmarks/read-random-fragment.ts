e4f93819f320f9691f2c509a917c7d13a1fac9fa
Andrej Szalma <andy.szalma@gmail.com>
2022-07-21 12:49:23 +0100

    formating

--- benchmarks/read-random-fragment.ts
+++ benchmarks/read-random-fragment.ts
@@ -11,23 +11,24 @@ export default class RandomRead extends Benchmark {
   fragment: Fragment;
   fragmentRes: object;
 
-  async setup() {    
+  async setup() {
     // Expand response
     this.example.response = populateResponse(this.example.response);
     // Write to cache
     await this.client.write(this.example);
 
-    if("fragmentPath" in this.example.fragment) {
+    if ('fragmentPath' in this.example.fragment) {
       // find all responses which match the fragmentPath, pick a random one
       // and save it
-      const responsePool = this.example.fragment
-        .fragmentPath.split(".").reduce((a, b) => a[b], this.example.response)       
-      const tmp = responsePool[Math.floor(Math.random() * responsePool.length)]  
+      const responsePool = this.example.fragment.fragmentPath
+        .split('.')
+        .reduce((a, b) => a[b], this.example.response);
+      const tmp = responsePool[Math.floor(Math.random() * responsePool.length)];
       this.fragment = {
         typename: tmp.__typename,
         id: tmp.id,
-        response: tmp
-      }    
+        response: tmp,
+      };
     }
   }
 
