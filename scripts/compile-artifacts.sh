9b35bf5dcc766d2df50c3b95114d9b7566cdb411
Andrej Szalma <andy.szalma@gmail.com>
2022-07-28 09:39:59 +0100

    fixed artifact compilation to accept queries with any names and fragments

--- scripts/compile-artifacts.sh
+++ scripts/compile-artifacts.sh
@@ -13,9 +13,18 @@ for DIRECTORY in $DIRECTORIES; do
   mkdir $DIRECTORY/__artifacts__
   # compile artifacts
   yarn relay $DIRECTORY/relay.config.json
+  
   # remove raw query, move generated root artifact and rename
   rm $DIRECTORY/operation.gql.ts
-  mv $DIRECTORY/__artifacts__/operationQuery.graphql.ts $DIRECTORY/relayArtifact.graphql.ts
+  # find operation artefacts names and move them
+  OPERATION=$(find $DIRECTORY/__artifacts__/ -name 'operation*Query.graphql.ts' -maxdepth 1 | head -n 1)
+  if [[ ! -z "$OPERATION" ]]; then
+    mv $DIRECTORY/__artifacts__/${OPERATION##*/} $DIRECTORY/relayArtifact.graphql.ts
+  fi
+  # TODO: Find out if we actually need these remaining artifacts, or they can be deleted
+  # remove remaining fragment artifacts that we don't need
+  rm $DIRECTORY/__artifacts__/operation*.graphql.ts
+
   # remove raw fragment, move generated fragment and rename
   rm $DIRECTORY/fragment.gql.ts
   # find fragment artefacts names and move them
@@ -27,6 +36,7 @@ for DIRECTORY in $DIRECTORIES; do
   if [[ ! -z "$FRAGMENT" ]]; then
     mv $DIRECTORY/__artifacts__/${FRAGMENT##*/} $DIRECTORY/fragment_relayArtifact.graphql.ts
   fi
+
   # remove all temp versions of partial relay artifacts and replace them with the compiled ones
   rm -rf $DIRECTORY/__partials__/relay/*
   mv $DIRECTORY/__artifacts__/* $DIRECTORY/__partials__/relay/
