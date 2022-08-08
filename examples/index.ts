8c821a51f0af4ff22531306b1e40b9abf7055631
Andrej Szalma <andy.szalma@gmail.com>
2022-08-08 13:19:31 +0100

    fixed variables functionality in queries

--- examples/index.ts
+++ examples/index.ts
@@ -6,75 +6,80 @@ import { RawExample, Fragment, restructurePartialExamples } from '../src';
 const exampleContext = require.context(
   '.',
   true,
-  /(metadata\.json|operation\.gql|response\.json|schema\.gql|relayArtifact\.graphql\.ts|fragment\.gql)$/,
+  /(metadata\.json|operation\.gql|response\.json|schema\.gql|relayArtifact\.graphql\.ts|fragment\.gql|variables\.json)$/,
 );
 
-const partialContext = require.context(
-  '.',
-  true,
-  /(partial.+\.gql|partial.+Query\.graphql\.ts)$/,
-)
+const partialContext = require.context('.', true, /(partial.+\.gql|partial.+Query\.graphql\.ts)$/);
 
 const examplesByDirname: any = {};
 // Walk all asset files, and group them into examples (by dirname).
 exampleContext.keys().forEach(assetPath => {
-  const examplePath = path.dirname(assetPath).replace(/(\.|\/)/gm,"");
+  const examplePath = path.dirname(assetPath).replace(/(\.|\/)/gm, '');
   if (!examplesByDirname[examplePath]) {
     examplesByDirname[examplePath] = {};
   }
 
   let assetType = path.basename(assetPath, path.extname(assetPath));
   // Remove the secondary ".graphql" extension from relayArtifact
-  assetType = assetType.replace(".graphql", "")
-  const assetContent = exampleContext(assetPath)
+  assetType = assetType.replace('.graphql', '');
+  const assetContent = exampleContext(assetPath);
 
   if (assetType == 'metadata') {
     examplesByDirname[examplePath] = { ...examplesByDirname[examplePath], ...assetContent };
   } else if (assetType == 'fragment') {
-    examplesByDirname[examplePath][assetType] = {operation: assetContent}
-  } else if (assetType == 'fragment_relayArtifact'){
-    if('fragment' in examplesByDirname[examplePath])
-      examplesByDirname[examplePath].fragment = {...examplesByDirname[examplePath].fragment, relayArtifact: assetContent.default}
-    else
-      examplesByDirname[examplePath].fragment = {relayArtifact: assetContent.default}
-  } else if (assetType == 'fragmentOwner_relayArtifact'){
-    if('fragment' in examplesByDirname[examplePath])
-      examplesByDirname[examplePath].fragment = {...examplesByDirname[examplePath].fragment, ownerRelayArtifact: assetContent.default}
-    else
-      examplesByDirname[examplePath].fragment = {ownerRelayArtifact: assetContent.default} 
+    examplesByDirname[examplePath][assetType] = { operation: assetContent };
+  } else if (assetType == 'fragment_relayArtifact') {
+    if ('fragment' in examplesByDirname[examplePath])
+      examplesByDirname[examplePath].fragment = {
+        ...examplesByDirname[examplePath].fragment,
+        relayArtifact: assetContent.default,
+      };
+    else examplesByDirname[examplePath].fragment = { relayArtifact: assetContent.default };
+  } else if (assetType == 'fragmentOwner_relayArtifact') {
+    if ('fragment' in examplesByDirname[examplePath])
+      examplesByDirname[examplePath].fragment = {
+        ...examplesByDirname[examplePath].fragment,
+        ownerRelayArtifact: assetContent.default,
+      };
+    else examplesByDirname[examplePath].fragment = { ownerRelayArtifact: assetContent.default };
   } else {
     examplesByDirname[examplePath][assetType] = assetContent;
   }
 });
 
-// Go through all partials and group them by example 
+// Go through all partials and group them by example
 partialContext.keys().forEach(assetPath => {
-  const partialBaseDir = path.dirname(assetPath.replace(/(^(\.\/)|^\.|^\/)/gm, "")).split("/")[0];
+  const partialBaseDir = path.dirname(assetPath.replace(/(^(\.\/)|^\.|^\/)/gm, '')).split('/')[0];
   if (!('rawPartials' in examplesByDirname[partialBaseDir])) {
     examplesByDirname[partialBaseDir].rawPartials = {};
   }
 
   let assetType = path.basename(assetPath, path.extname(assetPath));
   // Remove the secondary ".graphql" extension from relayArtifact
-  assetType = assetType.replace(".graphql", "")
-  const assetContent = partialContext(assetPath)
+  assetType = assetType.replace('.graphql', '');
+  const assetContent = partialContext(assetPath);
 
-  if (!examplesByDirname[partialBaseDir].rawPartials[assetType.replace("Query", "")]) {
-    examplesByDirname[partialBaseDir].rawPartials[assetType.replace("Query", "")] = {};
+  if (!examplesByDirname[partialBaseDir].rawPartials[assetType.replace('Query', '')]) {
+    examplesByDirname[partialBaseDir].rawPartials[assetType.replace('Query', '')] = {};
   }
 
-  if (assetType.includes("Query")) {
-    examplesByDirname[partialBaseDir].rawPartials[assetType.replace("Query", "")]["relayArtifact"] = assetContent;
+  if (assetType.includes('Query')) {
+    examplesByDirname[partialBaseDir].rawPartials[assetType.replace('Query', '')][
+      'relayArtifact'
+    ] = assetContent;
   } else {
-    examplesByDirname[partialBaseDir].rawPartials[assetType]["operation"] = assetContent;
+    examplesByDirname[partialBaseDir].rawPartials[assetType]['operation'] = assetContent;
   }
-})
+});
 
 const examples: RawExample[] = [];
 for (const dirname of Object.keys(examplesByDirname)) {
-  if ("fragmentPath" in examplesByDirname[dirname]) {
-    examplesByDirname[dirname].fragment = {...examplesByDirname[dirname].fragment, fragmentPath: examplesByDirname[dirname].fragmentPath}
-    delete examplesByDirname[dirname].fragmentPath
+  if ('fragmentPath' in examplesByDirname[dirname]) {
+    examplesByDirname[dirname].fragment = {
+      ...examplesByDirname[dirname].fragment,
+      fragmentPath: examplesByDirname[dirname].fragmentPath,
+    };
+    delete examplesByDirname[dirname].fragmentPath;
   }
 
   const example = examplesByDirname[dirname];
@@ -94,6 +99,6 @@ function isRawExample(value: any): value is RawExample {
   return value.title && value.operation && value.response && value.schema;
 }
 
-console.log("Examples", examples)
+console.log('Examples', examples);
 
 export = examples;
